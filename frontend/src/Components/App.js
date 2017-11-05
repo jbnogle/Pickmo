eaca82fb30ea45605e7d9c8caef37848b09953d2
asnewman <ashznewman@gmail.com>
2017-11-05 14:11:00 -0800

    LED trigger button

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 import '../css/App.css';
+import axios from 'axios';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
-  }
+   ledTrigger() {
+      axios.get('https://p77rg9n9za.execute-api.us-west-2.amazonaws.com/stage1/control/1')
+         .then(res => {
+            console.log("API CALL - SUCCESS: led trigger")
+         })
+         .catch(res => {
+            console.log("API CALL - FAIL: led trigger")
+         })
+   }
+   render() {
+      return (
+         <div className="App">
+            <div className="container-fluid">
+               <div className="col">
+                  <button type="button" className="btn btn-primary"
+                     onClick={this.ledTrigger}>
+                     LED Trigger
+                  </button>
+               </div>
+            </div>
+         </div>
+      );
+   }
 }
 
 export default App;
